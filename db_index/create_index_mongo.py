8b71ee7fd9eb87f6f664cd80722c3c4fde7340e6
Muneeb Ali <muneeb@ali.vc>
2015-04-30 16:50:04 -0400

    separated storing blocks/tx in DB from processing the info for UTXO

--- db_index/create_index_mongo.py
+++ db_index/create_index_mongo.py
@@ -25,8 +25,14 @@ db = c['namecoin_index']
 mongo_blocks = db.blocks
 mongo_tx = db.tx
 
+#outputs are a superset of utxo (unspent outputs)
+mongo_utxo = db.utxo
+mongo_inputs = db.inputs
+
 mongo_blocks.ensure_index('block_num')
 mongo_tx.ensure_index('tx_hash')
+mongo_utxo.ensure_index('id')
+mongo_inputs.ensure_index('id')
 
 
 # -----------------------------------
@@ -38,46 +44,122 @@ class DecimalEncoder(json.JSONEncoder):
 
 
 # -----------------------------------
-def write_blocks_and_tx():
+def spend_utxo(id):
 
-    start_block_num, end_block_num = 1, namecoind.getblockcount()
+    input = mongo_inputs.find_one({"id": id})
 
-    error_blocks = []
-    error_tx = []
+    if input is not None:
+        print "Spending UTXO: " + id
+        mongo_utxo.remove({"id": id})
+    else:
+        print "UTXO still unspent"
+        #print id
 
-    start_block_num, end_block_num = 1, 100
+
+# -----------------------------------
+def save_block(block_num):
+
+    print "Procesing block %d" % block_num
+    block = namecoind.getblockbycount(block_num)
+
+    block_entry = {}
+
+    block_entry['block_num'] = block_num
+    block_entry['block_data'] = block
+
+    block_entry = json.loads(json.dumps(block_entry, cls=DecimalEncoder))
+
+    mongo_blocks.insert(block_entry)
+
+    if 'tx' in block:
+        txs = block['tx']
+        #print "Found %d transactions" % len(txs)
+        for tx in txs:
+            #print "Transaction ID: " + tx
+
+            raw_tx = namecoind.getrawtransaction(tx)
+            tx_data = namecoind.decoderawtransaction(raw_tx)
+
+            tx_entry = {}
+            tx_entry['tx_hash'] = str(tx)
+            tx_entry['tx_data'] = tx_data
+
+            tx_entry = json.loads(json.dumps(tx_entry, cls=DecimalEncoder))
+
+            mongo_tx.insert(tx_entry)
+
+
+# -----------------------------------
+def process_block(block_num):
+    ''' Processes DB data
+    '''
+
+    print "Procesing block %d" % block_num
+    block = mongo_blocks.find_one({'block_num': block_num})
+
+    block_data = block['block_data']
+
+    if 'tx' in block_data:
+        txs = block_data['tx']
+
+        for tx in txs:
+            tx_hash = tx
+            tx = mongo_tx.find_one({'tx_hash': tx_hash})
+
+            tx_data = tx['tx_data']
+
+            if 'vin' in tx_data:
+
+                for input in tx_data['vin']:
+
+                    if 'txid' in input:
+                        id = input['txid'] + '_' + str(input['vout'])
+
+                        new_input = {}
+
+                        new_input['id'] = id
+                        new_input['data'] = input
+
+                        mongo_inputs.insert(new_input)
+
+                        # spend the output
+                        spend_utxo(id)
+
+            if 'vout' in tx_data:
+
+                for output in tx_data['vout']:
+
+                    id = tx_hash + '_' + str(output['n'])
+
+                    new_output = {}
+                    new_output['id'] = id
+                    new_output['data'] = output
+
+                    mongo_utxo.insert(new_output)
+
+
+# -----------------------------------
+def process_blocks_from_beginning():
+
+    #start_block_num, end_block_num = 1, namecoind.getblockcount()
+    start_block_num, end_block_num = 1, 1000
+
     for block_num in range(start_block_num, end_block_num + 1):
+        save_block(block_num)
+        process_block(block_num)
 
-        print "Procesing block %d" % block_num
-        block = namecoind.getblockbycount(block_num)
 
-        block_entry = {}
+# -----------------------------------
+def check_all_utxo():
 
-        block_entry['block_num'] = block_num
-        block_entry['block_data'] = block
+    for utxo in mongo_utxo.find():
 
-        block_entry = json.loads(json.dumps(block_entry, cls=DecimalEncoder))
-
-        mongo_blocks.insert(block_entry)
-
-        if 'tx' in block:
-            txs = block['tx']
-            #print "Found %d transactions" % len(txs)
-            for tx in txs:
-                #print "Transaction ID: " + tx
-
-                raw_tx = namecoind.getrawtransaction(tx)
-                tx_data = namecoind.decoderawtransaction(raw_tx)
-
-                tx_entry = {}
-                tx_entry['tx_hash'] = str(tx)
-                tx_entry['tx_data'] = tx_data
-
-                tx_entry = json.loads(json.dumps(tx_entry, cls=DecimalEncoder))
-
-                mongo_tx.insert(tx_entry)
+        spend_utxo(utxo)
 
 # -----------------------------------
 if __name__ == '__main__':
 
-    write_blocks_and_tx()
+    #process_blocks_from_beginning()
+    check_all_utxo()
+    #write_unspents()
+    #spend_utxo()
